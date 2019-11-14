17ad4de07b9c702b0671c0d0d803793f2856f680
s-zengju <s-zengju@53S500038190.it.bsd405.org>
2019-11-14 09:24:43 -0800

    Submitting Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -9,9 +9,15 @@ public class FracCalc {
     public static void main(String[] args)
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
+    	System.out.print("What do you want to calculate? ");
     	Scanner sc = new Scanner(System.in);
     	String expression = sc.nextLine();
-    	System.out.println(produceAnswer(expression));
+    	while (!expression.equals("quit")) {
+    		System.out.println(produceAnswer(expression));
+        	System.out.print("What do you want to calculate? ");
+    		expression = sc.nextLine();
+    	}
+        System.out.println("End");
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -31,13 +37,59 @@ public class FracCalc {
     	int secSpace = input.indexOf(" ", firstSpace + 1);
     	String opr = input.substring(firstSpace + 1, secSpace);
     	String secOp = input.substring(secSpace + 1);
-//    	for (int i = 0; i < firstSpace; i++) {
-//    		firstOp += input.charAt(i);
-//    	}
-//    	int firstOp = input.charAt(firstSpace + 1);
     	
-        return secOp;
+        return "whole:" + wholePart(secOp) + " numerator:" + numerator(secOp) + " denominator:" + denominator(secOp);
     }
+    
+    public static int underscore(String operand) {
+    	int underscore = operand.indexOf("_");
+    	return underscore;
+    }
+    
+    public static int fracLine(String operand) {
+    	int fracLine = operand.indexOf("/");
+    	return fracLine;
+    }
+    
+    public static String wholePart(String operand) {
+    	int underscore = underscore(operand);
+    	int fracLine = fracLine(operand);
+    	String whole = "";
+    	if (underscore != -1 && fracLine != -1) {
+        	whole = operand.substring(0, underscore);
+    	} else if (underscore == -1 && fracLine == -1){
+    		whole = operand;
+    	} else {
+    		whole = "0";
+    	}
+    	return whole;
+    }
+    
+    public static String numerator(String operand) {
+    	int underscore = underscore(operand);
+    	int fracLine = fracLine(operand);
+    	String num = "";
+    	if (underscore != -1 && fracLine != -1) {
+        	num = operand.substring(underscore + 1, fracLine);
+    	} else if (underscore == -1 && fracLine == -1){
+    		num = "0";
+    	} else {
+    		num = operand.substring(0, fracLine);
+    	}
+    	return num;
+    }
+    
+    public static String denominator(String operand) {
+    	int underscore = underscore(operand);
+    	int fracLine = fracLine(operand);
+    	String den = "";
+    	if (underscore == -1 && fracLine == -1) {
+    		den = "1";
+    	} else {
+    		den = operand.substring(fracLine + 1);
+    	}
+    	return den;
+    	}
 
     // TODO: Fill in the space below with any helper methods that you think you will need
 
